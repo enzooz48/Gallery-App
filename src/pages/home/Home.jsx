03d8379c5ea2b5211a09ce3c265ebbb1ccac3710
EnZooZ <enzooz4889@gmail.com>
2022-08-25 23:53:53 +0700

    Programs Component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Header from '../../components/Header';
+import Programs from '../../components/Programs';
 import './Home.css';
 
 const Home = () => {
 	return (
 		<>
 			<Header />
+			<Programs />
 		</>
 	);
 };

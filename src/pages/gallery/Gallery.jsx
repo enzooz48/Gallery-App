173db2127d98bbf9fd2f0a251b33a057f04c913c
EnZooZ <enzooz4889@gmail.com>
2022-08-26 22:25:23 +0700

    Add Gallery Component

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,7 +1,36 @@
+import Header from '../../components/Header';
+import HeaderImage from '../../assets/images/header_bg_3.jpg';
+
 import './Gallery.css';
 
 const Gallery = () => {
-	return <div>Gallery</div>;
+	const galleryLength = 15;
+	const images = [];
+
+	for (let i = 1; i <= galleryLength; i++) {
+		images.push(require(`../../assets/images/gallery${i}.jpg`));
+	}
+
+	return (
+		<>
+			<Header title="Our Gallery" image={HeaderImage}>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae
+				impedit rem temporibus natus rerum et magni odit eligendi, saepe
+				consequatur!
+			</Header>
+			<section className="gallery">
+				<div className="container gallery__container">
+					{images.map((image, index) => {
+						return (
+							<article key={index}>
+								<img src={image} alt={`GalleryImage ${index + 1}`} />
+							</article>
+						);
+					})}
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default Gallery;
